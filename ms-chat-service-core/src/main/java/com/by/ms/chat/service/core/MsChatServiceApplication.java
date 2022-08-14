e4526d5edd879aabf25bddf8f98fc70968b475bc
boyulin <1404916254@qq.com>
2022-08-14 15:08:41 +0800

    initial

---
+++ ms-chat-service-core/src/main/java/com/by/ms/chat/service/core/MsChatServiceApplication.java
@@ -0,0 +1,17 @@
+package com.by.ms.chat.service.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = "com.by.ms.chat.service")
+public class MsChatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsChatServiceApplication.class, args);
+    }
+
+}
